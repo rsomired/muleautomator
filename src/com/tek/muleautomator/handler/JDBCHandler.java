e012ce5d337877436646d963a307606cbd968fa2
ukaranam <ukaranam@HSC-2Q8T6BS.allegisgroup.com>
2017-08-17 17:21:35 +0530

    added jdbc handler

--- src/com/tek/muleautomator/handler/JDBCHandler.java
+++ src/com/tek/muleautomator/handler/JDBCHandler.java
@@ -18,7 +18,23 @@ public class JDBCHandler {
 			JDBCElement.JDBCCallActivity jdbcCallActivity = new JDBCElement.JDBCCallActivity(activityElement.getTargetNode());
 			jdbcService.jdbcCallProcedure(muleProjectLocation, jdbcCallActivity, flowElement);
 			break;
+		case "com.tibco.plugin.jdbc.JDBCQueryActivity":
+			System.out.println("com.tibco.plugin.jdbc.JDBCQueryActivity-----The JDBC Call Procedure activity calls a database procedure using the specified JDBC connection");
+			JDBCElement.JDBCQueryActivity jdbcQueryActivity = new JDBCElement.JDBCQueryActivity(activityElement.getTargetNode());
+			jdbcService.jdbcSelect(muleProjectLocation, jdbcQueryActivity, flowElement);
+			break;
+		case "com.tibco.plugin.jdbc.JDBCUpdateActivity":
+			System.out.println("com.tibco.plugin.jdbc.JDBCUpdateActivity-----The JDBC Call Procedure activity calls a database procedure using the specified JDBC connection");
+			JDBCElement.JDBCUpdateActivity jdbcUpdateActivity = new JDBCElement.JDBCUpdateActivity(activityElement.getTargetNode());
+			//jdbcService.jdbcInsert(muleProjectLocation, jdbcUpdateActivity, flowElement);
+			jdbcService.jdbcUpdate(muleProjectLocation, jdbcUpdateActivity, flowElement);
+			//jdbcService.jdbcDelete(muleProjectLocation, jdbcUpdateActivity, flowElement);
+			break;	
+		case "com.tibco.plugin.jdbc.JDBCGeneralActivity":
+			System.out.println("com.tibco.plugin.jdbc.JDBCGeneralActivity-----The JDBC Call Procedure activity calls a database procedure using the specified JDBC connection");
+			break;	
 		}
 	}
+	
 
 }
