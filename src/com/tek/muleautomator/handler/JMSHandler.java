61fec6bf52f989a0fcd72c91825aed9c6750b8b2
Ravikanth Somireddy <ravikanth.somireddy@tele2.com>
2017-08-30 17:45:19 +0530

    Added jms mule flow in service and cases in handler.

--- src/com/tek/muleautomator/handler/JMSHandler.java
+++ src/com/tek/muleautomator/handler/JMSHandler.java
@@ -1,6 +1,8 @@
 package com.tek.muleautomator.handler;
 
 import com.tek.muleautomator.element.ActivityElement;
+import com.tek.muleautomator.element.JMSElement;
+
 import org.w3c.dom.Element;
 
 import com.tek.muleautomator.service.JMSService;
@@ -16,19 +18,28 @@ public class JMSHandler {
 		switch(activityType){
 		case "com.tibco.plugin.jms.JMSQueueEventSource":
 			System.out.println("com.tibco.plugin.jms.JMSQueueEventSource-----This actvity will be used to receive jms messages from jms/tibco ems servers");
-			jmsService.jmsSubscribe(muleConfigPath, flowElement);
+			JMSElement.JMSQueueEventSource jmsQueueEventSource = new JMSElement.JMSQueueEventSource(activityElement.getTargetNode());
+			jmsService.jmsQueueReceiver(muleConfigPath, flowElement, jmsQueueEventSource);
 			break;
 		case "com.tibco.plugin.jms.JMSQueueSendActivity":
 			System.out.println("This is used to send jms messages to jms/tibco ems servers this is not a blocking activity i.e it will not wait for response from consumer");
+			JMSElement.JMSQueueSendActivity jmsQueueSendActivity = new JMSElement.JMSQueueSendActivity(activityElement.getTargetNode());
+			jmsService.jmsQueueSender(muleConfigPath, flowElement, jmsQueueSendActivity);
 			break;
 		case "com.tibco.plugin.jms.JMSQueueRequestReplyActivity":
 			System.out.println("This activity is used to send request and wait for response, in request reply scenarios we use this activity");
+			JMSElement.JMSQueueRequestReplyActivity jmsQueueRequestReplyActivity = new JMSElement.JMSQueueRequestReplyActivity(activityElement.getTargetNode());
+			jmsService.jmsQueueRequestor(muleConfigPath, flowElement, jmsQueueRequestReplyActivity);
 			break;
 		case "com.tibco.plugin.jms.JMSTopicPublishActivity":
 			System.out.println("This is used to send jms messages to jms/tibco ems servers this is not a blocking activity i.e it will not wait for response from consumer");
+			JMSElement.JMSTopicPublishActivity jmsTopicPublishActivity = new JMSElement.JMSTopicPublishActivity(activityElement.getTargetNode());
+			jmsService.jmsTopicPublisher(muleConfigPath, flowElement, jmsTopicPublishActivity);
 			break;
 		case "com.tibco.plugin.jms.JMSTopicRequestReplyActivity":
 			System.out.println("This activity is used to send request and wait for response, In request reply scenarios we use this activity");
+			JMSElement.JMSTopicRequestReplyActivity jmsTopicRequestReplyActivity = new JMSElement.JMSTopicRequestReplyActivity(activityElement.getTargetNode());
+			jmsService.jmsTopicRequestor(muleConfigPath, flowElement, jmsTopicRequestReplyActivity);
 			break;
 		case "com.tibco.plugin.jms.JMSTopicSignalInActivity":
 			System.out.println("This acitivity is used in scenarios where we have asyncronous pattern.This actvity will wait for messages on a topic");
@@ -41,6 +52,8 @@ public class JMSHandler {
 			break;
 		case "com.tibco.plugin.jms.JMSReplyActivity":
 			System.out.println("com.tibco.plugin.jms.JMSReplyActivity-----This activity is used while impelementing synchronous pattern, inorder to send response back to requestor");
+			JMSElement.JMSReplyActivity jmsReplyActivity = new JMSElement.JMSReplyActivity(activityElement.getTargetNode());
+			jmsService.replyToJmsMessage(muleConfigPath, flowElement, jmsReplyActivity);
 			break;
 		
 		}
--- src/com/tek/muleautomator/service/JMSService.java
+++ src/com/tek/muleautomator/service/JMSService.java
@@ -12,6 +12,13 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import com.tek.muleautomator.element.JMSElement.JMSQueueEventSource;
+import com.tek.muleautomator.element.JMSElement.JMSQueueRequestReplyActivity;
+import com.tek.muleautomator.element.JMSElement.JMSQueueSendActivity;
+import com.tek.muleautomator.element.JMSElement.JMSReplyActivity;
+import com.tek.muleautomator.element.JMSElement.JMSTopicPublishActivity;
+import com.tek.muleautomator.element.JMSElement.JMSTopicRequestReplyActivity;
+import com.tek.muleautomator.util.MuleAutomatorUtil;
 import com.tek.muleautomator.util.MuleConfigConnection;
 
 public class JMSService {
@@ -46,17 +53,111 @@ public class JMSService {
 			jmsOutBound.setAttribute("doc:name", "JMSSample");
 			jmsOutBound.setAttribute("exchange-pattern", "request-response");
 			flow.appendChild(jmsOutBound);
-
-			Element loggerElement=doc.createElement("logger");
-			loggerElement.setAttribute("message", "#[payload]");
-			loggerElement.setAttribute("level", "INFO");
-			loggerElement.setAttribute("doc:name", "Logger");
-			flow.appendChild(loggerElement);
+			MuleAutomatorUtil.loggerElement(muleConfigPath, flow);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
 	}
 
+	public void jmsQueueReceiver(String muleConfigPath, Element flow, JMSQueueEventSource jmsQueueEventSource) {
+		try {
+			Document doc = MuleConfigConnection.getDomObj(muleConfigPath);
+			if(isJmsConfigRequired(muleConfigPath)){
+				jmsConfiguration(muleConfigPath, flow);
+			}
+			Element jmsQueueReceiver =doc.createElement("jms:inbound-endpoint");
+			jmsQueueReceiver.setAttribute("queue", "sample");
+			jmsQueueReceiver.setAttribute("connector-ref", "Active_MQ");
+			jmsQueueReceiver.setAttribute("doc:name", "JMSSample");
+			flow.appendChild(jmsQueueReceiver);
+			MuleAutomatorUtil.loggerElement(muleConfigPath, flow);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public void jmsQueueSender(String muleConfigPath, Element flow, JMSQueueSendActivity jmsQueueSendActivity) {
+		try {
+			Document doc = MuleConfigConnection.getDomObj(muleConfigPath);
+			if(isJmsConfigRequired(muleConfigPath)){
+				jmsConfiguration(muleConfigPath, flow);
+			}
+			Element jmsQueueSender =doc.createElement("jms:outbound-endpoint");
+			jmsQueueSender.setAttribute("queue", "sample");
+			jmsQueueSender.setAttribute("connector-ref", "Active_MQ");
+			jmsQueueSender.setAttribute("doc:name", "JMSSample");
+			flow.appendChild(jmsQueueSender);
+			MuleAutomatorUtil.loggerElement(muleConfigPath, flow);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public void jmsQueueRequestor(String muleConfigPath, Element flow, JMSQueueRequestReplyActivity jmsQueueRequestReplyActivity) {
+		try {
+			Document doc = MuleConfigConnection.getDomObj(muleConfigPath);
+			if(isJmsConfigRequired(muleConfigPath)){
+				jmsConfiguration(muleConfigPath, flow);
+			}
+			Element jmsQueueReceiver =doc.createElement("jms:outbound-endpoint");
+			jmsQueueReceiver.setAttribute("queue", "sample");
+			jmsQueueReceiver.setAttribute("connector-ref", "Active_MQ");
+			jmsQueueReceiver.setAttribute("doc:name", "JMSSample");
+			jmsQueueReceiver.setAttribute("exchange-pattern", "request-response");
+			flow.appendChild(jmsQueueReceiver);
+			MuleAutomatorUtil.loggerElement(muleConfigPath, flow);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public void jmsTopicPublisher(String muleConfigPath, Element flow, JMSTopicPublishActivity jmsTopicPublishActivity) {
+		try {
+			Document doc = MuleConfigConnection.getDomObj(muleConfigPath);
+			if(isJmsConfigRequired(muleConfigPath)){
+				jmsConfiguration(muleConfigPath, flow);
+			}
+			Element jmsTopicPublisher =doc.createElement("jms:outbound-endpoint");
+			jmsTopicPublisher.setAttribute("connector-ref", "Active_MQ");
+			jmsTopicPublisher.setAttribute("doc:name", "JMSSample");
+			jmsTopicPublisher.setAttribute("topic", "ActiveMQ.Advisory.TempQueue");
+			flow.appendChild(jmsTopicPublisher);
+			MuleAutomatorUtil.loggerElement(muleConfigPath, flow);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public void jmsTopicRequestor(String muleConfigPath, Element flow, JMSTopicRequestReplyActivity jmsTopicRequestReplyActivity) {
+		try {
+			Document doc = MuleConfigConnection.getDomObj(muleConfigPath);
+			if(isJmsConfigRequired(muleConfigPath)){
+				jmsConfiguration(muleConfigPath, flow);
+			}
+			Element jmsQueueReceiver =doc.createElement("jms:outbound-endpoint");
+			jmsQueueReceiver.setAttribute("connector-ref", "Active_MQ");
+			jmsQueueReceiver.setAttribute("doc:name", "JMSSample");
+			jmsQueueReceiver.setAttribute("topic", "ActiveMQ.Advisory.TempQueue");
+			jmsQueueReceiver.setAttribute("exchange-pattern", "request-response");
+			flow.appendChild(jmsQueueReceiver);
+			MuleAutomatorUtil.loggerElement(muleConfigPath, flow);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public void replyToJmsMessage(String muleConfigPath, Element flow, JMSReplyActivity jmsReplyActivity) {
+		try {
+			Document doc = MuleConfigConnection.getDomObj(muleConfigPath);
+			Element replyToJmsMessage =doc.createElement("set-payload");
+			replyToJmsMessage.setAttribute("value", "#[payload]");
+			replyToJmsMessage.setAttribute("doc:name", "Set Payload");
+			flow.appendChild(replyToJmsMessage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
 	public boolean isJmsConfigRequired(String muleConfigPath) throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder docBuilder;
