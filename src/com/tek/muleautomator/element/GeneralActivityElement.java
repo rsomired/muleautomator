b5a0f8bf9111d5b81333f873fe45fab88d1edade
Ashish Gupta <asgupta@teksystems.com>
2017-09-18 17:46:20 +0530

    General Activities Element class: Empty

---
+++ src/com/tek/muleautomator/element/GeneralActivityElement.java
@@ -0,0 +1,32 @@
+package com.tek.muleautomator.element;
+
+public class GeneralActivityElement {
+
+	public static class AssignActivity {
+		
+	}
+	
+	public static class CatchActivity {
+		
+	}
+	
+	public static class GetSharedVariableActivity {
+		
+	}
+	
+	public static class InspectorActivity {
+		
+	}
+	
+	public static class SharedVariableActivity{
+		
+	}
+	
+	public static class SleepActivity {
+		
+	}
+	
+	public static class TimerActivity {
+		
+	}
+}
